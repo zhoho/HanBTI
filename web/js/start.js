b8764e6a1c181ff0245c4fde73fa4933dd6f37e6
Zho <chlwlgh1011@naver.com>
2022-05-31 14:33:10 +0900

    question_set

--- web/js/start.js
+++ web/js/start.js
@@ -1,21 +1,45 @@
 const main = document.querySelector("#main_page");
 const qna = document.querySelector("#qna_page");
 
-function addAnswer(answerText)
+function addAnswer(answerText, qIdx)
 {
-    var a = document.querySelector('.answerList');
+    var a = document.querySelector('.answerBox');
     var answer = document.createElement('button');
+    answer.classList.add('answerList');
+    answer.classList.add('my-3');
+    answer.classList.add('py-3');
+    answer.classList.add('mx-auto');
+    answer.classList.add('fadeIn');
+    
+
     a.appendChild(answer);
     answer.innerHTML = answerText;
+
+    answer.addEventListener("click", function(){
+        var children = document.querySelectorAll('.answerList');
+        for(let i = 0; i < children.length; i++)
+        {
+            children[i].disabled = true;
+            children[i].style.WebkitAnimation = "fadeOut 0.75s";
+            children[i].style.animation = "fadeOut 0.75s";
+        }
+        setTimeout(() => {
+            for(let i = 0; i < children.length; i++)
+            {
+                children[i].style.display = 'none';
+            }
+          goNext(++qIdx);
+        }, 450);
+    },false);
 }
 
 function goNext(qIdx)
 {
-    var q = document.querySelector('.questionList');
+    var q = document.querySelector('.questionBox');
     q.innerHTML = qnaList[qIdx].q;
     for(let i in qnaList[qIdx].a)
     {
-        addAnswer(qnaList[qIdx].a[i].answer);
+        addAnswer(qnaList[qIdx].a[i].answer, qIdx);
     }
 }
 
