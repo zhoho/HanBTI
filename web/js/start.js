8702e0acd34a16eb36eb843c734eed602198bfd8
Zho <chlwlgh1011@naver.com>
2022-05-31 23:09:40 +0900

    value,key값 적용시켜서 결과나오게 하기 성공

--- web/js/start.js
+++ web/js/start.js
@@ -1,8 +1,75 @@
 const main = document.querySelector("#main_page");
 const qna = document.querySelector("#qna_page");
 const result = document.querySelector('#result_page');
-const question_num = 16;
+const question_num = 15;
+const select = [];
+const select_to_mbti = [];
 
+function calculate_result()
+{
+    var mbtiArray = [
+        {name : 'i', value : 0, key : 0},
+        {name : 'e', value : 0, key : 1},
+        {name : 'n', value : 0, key : 2},
+        {name : 's', value : 0, key : 3},
+        {name : 'f', value : 0, key : 4},
+        {name : 't', value : 0, key : 5},
+        {name : 'p', value : 0, key : 6},
+        {name : 'j', value : 0, key : 7},
+    ]
+    for(let i = 0; i < question_num; i++)
+    {
+        var target = qnaList[i].a[select[i]];
+        for(let j = 0; j < target.type.length; j++)
+        {
+            for(let k = 0; k < mbtiArray.length; k++)
+            {
+                if(target.type[j] === mbtiArray[k].name)
+                {
+                    mbtiArray[k].value += 1;
+                }
+            }
+        }
+    }
+    //console.log(mbtiArray);
+    if(mbtiArray[0].value >= mbtiArray[1].value)
+    {
+        select_to_mbti.push('I');
+    }  
+    else if(mbtiArray[0].value < mbtiArray[1].value)
+    {
+        select_to_mbti.push('E');
+    }
+
+    if(mbtiArray[2].value >= mbtiArray[3].value)
+    {
+        select_to_mbti.push('N');
+    }  
+    else if(mbtiArray[2].value < mbtiArray[3].value)
+    {
+        select_to_mbti.push('S');
+    }
+
+    if(mbtiArray[4].value >= mbtiArray[5].value)
+    {
+        select_to_mbti.push('F');
+    }  
+    else if(mbtiArray[4].value < mbtiArray[5].value)
+    {
+        select_to_mbti.push('T');
+    }
+
+    if(mbtiArray[6].value >= mbtiArray[7].value)
+    {
+        select_to_mbti.push('P');
+    }  
+    else if(mbtiArray[6].value < mbtiArray[7].value)
+    {
+        select_to_mbti.push('J');
+    }
+    console.log(select_to_mbti);
+
+}
 function goResult()
 {
     qna.style.WebkitAnimation = "fadeOut 1s";
@@ -14,9 +81,11 @@ function goResult()
         qna.style.display = "none";
         result.style.display = "block";
     }, 450)})
+    console.log(select);
+    calculate_result();
 }
 
-function addAnswer(answerText, qIdx)
+function addAnswer(answerText, qIdx, idx)
 {
     var a = document.querySelector('.answerBox');
     var answer = document.createElement('button');
@@ -39,6 +108,7 @@ function addAnswer(answerText, qIdx)
             children[i].style.animation = "fadeOut 0.75s";
         }
         setTimeout(() => {
+            select[qIdx] = idx;
             for(let i = 0; i < children.length; i++)
             {
                 children[i].style.display = "none";
@@ -50,7 +120,7 @@ function addAnswer(answerText, qIdx)
 
 function goNext(qIdx)
 {
-    if(qIdx+1 === question_num)
+    if(qIdx === question_num)
     {
         goResult();
         return;
@@ -59,7 +129,7 @@ function goNext(qIdx)
     q.innerHTML = qnaList[qIdx].q;
     for(let i in qnaList[qIdx].a)
     {
-        addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+        addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
     }
 }
 
